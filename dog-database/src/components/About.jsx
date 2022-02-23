ec912c752b3dc7ac5440f551f96229ba87131635
Alvin <awesomealvin360@gmail.com>
2022-02-22 21:17:26 -0800

    Changes for Home Page

---
+++ dog-database/src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="http://placehold.it/900x400"
+              alt=""
+            />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">About</h1>
+            <p>
+             Dog DataBase is an online dog center where users can access and and add, remove, or learn about a dog. We are driven by the growing number of dogs 
+             occupying animal shelters and wish to find homes for our canine companions. Here, you can browse and pick from a variety of dogs to adopt. 
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
