509a177a7c2ce72bb01633b5c78dece6086d62fc
Synerge360 <73147167+Synerge360@users.noreply.github.com>
2022-03-09 00:28:14 -0800

    Revert "Ahoan055/home page"

--- dog-database/src/components/Navigation.jsx
+++ dog-database/src/components/Navigation.jsx
@@ -2,41 +2,57 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from 'styled-components';
 
+
 function Navigation() {
   return (
     <div className="navigation">
       <nav className="navbar navbar-expand navbar-dark bg-dark">
+    
             <img src = {require("./download.png")}
-            width = "70"
+            //width = "70"
             height = "70"/>
         <div className="container">
+          
           <NavLink className="navbar-brand" to="/">
             <h1 class = "font-weight-normal-bold">Dog Database</h1>
           </NavLink>
+          
           <div>
-            <ul className="navbar-nav mx-auto">
+            <ul className="navbar-nav ml-auto">
               <li className="nav-item">
                 <NavLink className="nav-link" to="/">
-                <h1 class = "font-weight-normal-bold"> Home </h1>
+                <h1 class = "font-weight-normal-bold">| Home |</h1>
                   <span className="sr-only">(current)</span>
                 </NavLink>
                 
               </li>
+              
               <li className="nav-item">
                 <NavLink className="nav-link" to="/about">
-                <h1 class = "font-weight-normal-bold">About </h1>
+                <h1 class = "font-weight-normal-bold">About |</h1>
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink className="nav-link" to="/DogListings">
-                <h1 class = "font-weight-normal-bold">Dog Listings </h1>
+                <h1 class = "font-weight-normal-bold">DogListings |</h1>
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/AddDog">
+                <h1 class = "font-weight-normal-bold">Dog Add |</h1>
+                </NavLink> 
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/RemoveDog">
+                <h1 class = "font-weight-normal-bold">Dog Remove |</h1>
                 </NavLink>
               </li>
             </ul>
           </div>
         </div>
+       
             <img src = {require("./download.png")}
-            width = "70"
+            //width = "70"
             height = "70"/>
       </nav>
     </div>
