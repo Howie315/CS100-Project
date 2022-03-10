e4f078829a68145285de7df3b5e39863e203856a
Alvin <awesomealvin360@gmail.com>
2022-03-09 19:41:19 -0800

    changes to home

--- dog-database/src/components/DogListings.jsx
+++ dog-database/src/components/DogListings.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import DogListingsTiles from "./DogListingsTiles";
 import DogPost from "./DogPost";
 import Post from "./Post";
 import PostTile from "./PostTile";
@@ -8,20 +7,20 @@ function DogListings() {
   return (
     <div className="DogListings">
       <div class="container">
-        <div class="row align-items-center my-5">
+        <div class="row align-items-center my-0">
           <div class="col-lg-7">
-           
           </div>
-          <div class="col-lg-12">
-          
-            <h1 class="font-weight-light">DogListings</h1>
-        <DogPost/>
+          <div class="col-lg-5.5">
+
+            <h1 class="font-weight-normal-bold">Dog Listings</h1>
+            <DogPost/>
+
+            
           </div>
         </div>
       </div>
 
     </div>
-    
   );
 }
 
--- dog-database/src/components/index.js
+++ dog-database/src/components/index.js
@@ -2,7 +2,5 @@ export { default as Navigation } from "./Navigation";
 export { default as Home } from "./Home";
 export { default as About } from "./About";
 export { default as DogListings } from "./DogListings";
-export { default as AddDog } from "./AddDog";
-export { default as RemoveDog } from "./RemoveDog";
 export {default as DogPost} from "./DogPost";
 export {default as Post} from "./Post";
