7f62e9fd972923e7488181875b14b40f3dbafd55
Howie315 <nguyen.howie2010@gmail.com>
2022-03-10 10:36:57 -0800

    test

--- dog-database/src/App.test.js
+++ dog-database/src/App.test.js
@@ -7,6 +7,7 @@ import renderer from "react-test-renderer";
 import Navigation from "./components/Navigation";
 import About from "./components/About";
 import Home from "./components/Home";
+import Post from "./components/Post";
 it("renders correctly across screens", () => {
   const tree = renderer.create(<DogPost />).toJSON();
   expect(tree).toMatchSnapshot();
@@ -14,5 +15,9 @@ it("renders correctly across screens", () => {
 
 test("renders correctly", () => {
   const tree = renderer.create(<PostTile />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+test("renders correctly", () => {
+  const tree = renderer.create(<Post />).toJSON();
   expect(tree).toMatchSnapshot();
 });
--- dog-database/src/components/Post.js
+++ dog-database/src/components/Post.js
@@ -6,41 +6,6 @@ import { storage } from "../firebase";
 
 const Post = ({ match }) => {
   const [post, setPost] = useState({});
-  const [image, setImage] = useState(null);
-  const [url, setUrl] = useState("");
-  const [progress, setProgress] = useState(0);
-  const handleChange = (e) => {
-    if (e.target.files[0]) {
-      setImage(e.target.files[0]);
-    }
-  };
-
-  const handleUpload = () => {
-    const uploadTask = storage.ref(`images/${image.name}`).put(image);
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const progress = Math.round(
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        );
-        setProgress(progress);
-      },
-      (error) => {
-        console.log(error);
-      },
-      () => {
-        storage
-          .ref("images")
-          .child(image.name)
-          .getDownloadURL()
-          .then((url) => {
-            setUrl(url);
-          });
-      }
-    );
-  };
-
-  console.log("image: ", image);
 
   useEffect(() => {
     instance
